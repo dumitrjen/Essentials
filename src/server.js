0f51bb2dd762835e23d1ec8de47c7c2801f9695f
dumitrjen <DUM220139@spengergasse.at>
2025-09-22 15:43:19 +0200

    init commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// temp data einbinden
+const recipes = require('../assets/temp-data').recipes
+
+app.get('/recipes', (req, res) => {
+  res.json(recipes);
+})
+
+// GET recipes/id
+app.get('/recipes/:id', (req, res) => {
+    const id = req.params.id
+    const recipe = recipes.find((recipe) => recipe.id === id)
+    if(!recipe) 
+      res.status(404).send('Recipe not found')
+
+    res.json(recipe);
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
